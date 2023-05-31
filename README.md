1e1dd4192c1c4ff866d4c5c0f4ba2f8929630867
Amin Fakia <ameenofa@gmail.com>
2023-05-31 10:52:09 +0200

    added more documentation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Eye-Tracker
  Python Based Eye Tracker Software
+
+## Getting Started
+ - pip install -r requirements.txt
+ - python main.py
+
+## Prerequisites
+ - python 3.>
--- test.bat
+++ test.bat
@@ -1,1 +1,10 @@
+@echo off
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
 python main.py
+
+goto :EOF
+
+:errorNoPython
+echo.
+echo Error^: Python is not installed, make sure to install python 3.>
